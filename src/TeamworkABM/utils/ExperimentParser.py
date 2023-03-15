8dfc992d017561db2a902d4556dc10b28db52ff9
Prakash Manandhar <jsaaymi@gmail.com>
2023-03-15 13:04:56 -0400

    Add initial function prototypes

--- src/TeamworkABM/utils/ExperimentParser.py
+++ src/TeamworkABM/utils/ExperimentParser.py
@@ -2,22 +2,35 @@
 class ExperimentParser:
     """
         Parses experiment data from provided experiment folder and id.
+
+        Methods
+        -------
+        createTimeline(time_increment_s=10)
+            Starts process to create timeline and store in a local or remote database
     """
 
     __algorith_version = "1.0.0"
-    
-    def __init__(self, folder: str, id: str):
+
+    def __init__(self, folder: str, id: str, results_database: str):
         """
-        Parameters
-        ----------
-        folder : str
-            Top level folder where the experiment is stored.
-        id : str
-            The id or name of the experiment
+            Parameters
+            ----------
+            folder : str
+                Top level folder where the experiment is stored.
+            id : str
+                The id or name of the experiment
+            results_database: str
+                Pointer to a local or remote database where results are to be stored
         """
         self.__folder = folder
         self.__id = id
 
-    def createTimeline(self) -> str:
-        
+    def createTimeline(self, time_increment_s=10) -> str:
+        """Starts process to create timeline and store results in a local database
+            Returns
+            -------
+            str
+                a GUID representing process id that can be used to query status of 
+                job    
+        """
         return ""
